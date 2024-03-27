d8689ed7e0e62e90df9dea4832cf005dbd83f243
Anna Shatilova <anxet@mail.ru>
2024-03-27 19:35:44 +0300

    Template correction, adding common styles and fonts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* <header className="App-header">
+        <img
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,9 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
-  );
+  )
 }
-
-export default App;
