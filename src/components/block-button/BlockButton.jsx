9664a93c102529acc0c29fb1776c29ac83fdd55f
Anna Shatilova <anxet@mail.ru>
2024-04-02 14:59:22 +0300

    Counter done

--- src/components/block-button/BlockButton.jsx
+++ src/components/block-button/BlockButton.jsx
@@ -3,12 +3,36 @@ import * as S from './BlockButton.styles'
 
 export const BlockButton = () => {
   const [isChangeButton, setIsChangeButton] = useState(false)
-  
+  const [count, setCount] = useState(0)
+
+  const handlerCounterMinus = (quantity,event) => {
+    if(count === 0) return
+    event.nativeEvent.stopPropagation()
+    return setCount(quantity - 1)
+  }
+
+  const handlerCounterPlus = (quantity,event) => {
+    event.nativeEvent.stopPropagation()
+    return setCount(quantity + 1)
+  }
+
   return (
     <S.BlockButton>
       <S.ButtonChange onClick={() => setIsChangeButton(!isChangeButton)}>
         {isChangeButton ? (
-          <> </>
+          <S.ButtonCounter>
+            <S.CounterProduct onClickCapture={(event) => handlerCounterMinus(count, event)}>
+              <S.CounterSvg>
+                <use xlinkHref="/img/icons/sprite.svg#icon-minus" />
+              </S.CounterSvg>
+            </S.CounterProduct>
+            <S.CounterCenter>{count} штуки</S.CounterCenter>
+            <S.CounterProduct onClickCapture={(event) => handlerCounterPlus(count, event)}>
+            <S.CounterSvg>
+              <use xlinkHref="/img/icons/sprite.svg#icon-plus" />
+            </S.CounterSvg>
+            </S.CounterProduct>
+          </S.ButtonCounter>
         ) : (
           <S.ButtonBuy>
             <S.ButtonBuySvg>
--- src/components/block-button/BlockButton.styles.js
+++ src/components/block-button/BlockButton.styles.js
@@ -7,6 +7,35 @@ export const BlockButton = styled.div`
 `
 export const ButtonChange = styled.div``
 
+export const ButtonCounter = styled.div`
+  width: 284px;
+  height: 56px;
+  padding: 8px 12px 8px 16px;
+  border-radius: 8px;
+  background: #ffffff;
+  border: 2px solid #1551e5;
+  box-shadow: 0px 0px 6px 0px #bac4e0;
+  display: flex;
+  gap: 12px;
+`
+export const CounterProduct = styled.button`
+background-color: white;
+`
+
+export const CounterSvg = styled.svg`
+  width: 24px;
+  height: 24px;
+  margin-top: 8px;
+`
+
+export const CounterCenter = styled.div`
+  width: 184px;
+  font-size: 16px;
+  font-weight: 500;
+  text-align: center;
+  margin-top: 8px;
+`
+
 export const ButtonBuy = styled(Button)`
   width: 284px;
   height: 56px;
